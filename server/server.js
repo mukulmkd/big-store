594cecbf80c715cab99ec2056c26f589d70c3903
Mukul Kishore <mukul.kishore.2009@gmail.com>
2019-07-08 01:00:42 +0530

    All

--- server/server.js
+++ server/server.js
@@ -18,7 +18,7 @@ const db = require("./config/keys").mongoURI;
 // Connect to MongoDB
 mongoose
     .connect(
-        db,
+        process.env.MONGODB_URI || db,
         { useNewUrlParser: true }
     )
     .then(() => console.log("MongoDB successfully connected"))
@@ -34,9 +34,9 @@ app.use("/api/users", users);
 //Serve Static Assets if in Production
 if (process.env.NODE_ENV === 'production') {
     //Set static folder
-    app.use(express.static('client/build'));
+    app.use(express.static('../client/build'));
     app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+        res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
     });
 }
 
