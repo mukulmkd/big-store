61036f6b854b723770badfe508840e8f63947328
Mukul Kishore <mukul.kishore.2009@gmail.com>
2019-07-08 21:35:18 +0530

    Change

--- server/server.js
+++ server/server.js
@@ -39,8 +39,8 @@ if (process.env.NODE_ENV === 'production') {
     //Set static folder
     app.use(express.static('../client/build'));
     app.get('*', (req, res) => {
-        console.log(path.join(__dirname, 'client', 'build', 'index.html'));
-        res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+        // res.sendFile(path.join(__dirname, '..', 'client', 'build', 'index.html'));
+        res.sendFile(path.join(__dirname + "../client/build/index.html"));
     });
 }
 
