4f21dbfa4b43e60f03594ac851e2ca4522db4c97
Mukul Kishore <mukul.kishore.2009@gmail.com>
2019-07-08 23:06:20 +0530

    Lots of changes

--- server.js
+++ server.js
@@ -34,13 +34,13 @@ require("./config/passport")(passport);
 // Routes
 app.use("/api/users", users);
 
-//Serve Static Assets if in Production
+// Serve static assets if in production
 if (process.env.NODE_ENV === 'production') {
-    //Set static folder
-    app.use(express.static('../client/build'));
+    // Set static folder
+    app.use(express.static('client/build'));
+
     app.get('*', (req, res) => {
-        // res.sendFile(path.join(__dirname, '..', 'client', 'build', 'index.html'));
-        res.sendFile(path.resolve("../client/build/index.html"));
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
     });
 }
 
