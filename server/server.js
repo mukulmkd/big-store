f7d58c54e0a644e3dae4e998da58676199ded68e
Mukul Kishore <mukul.kishore.2009@gmail.com>
2019-07-08 22:49:06 +0530

    Changes

--- server/server.js
+++ server/server.js
@@ -37,10 +37,10 @@ app.use("/api/users", users);
 //Serve Static Assets if in Production
 if (process.env.NODE_ENV === 'production') {
     //Set static folder
-    app.use(express.static(__dirname + '../client/build'));
+    app.use(express.static('../client/build'));
     app.get('*', (req, res) => {
         // res.sendFile(path.join(__dirname, '..', 'client', 'build', 'index.html'));
-        res.sendFile(path.join(__dirname + "../client/build/index.html"));
+        res.sendFile(path.resolve("../client/build/index.html"));
     });
 }
 
